29eaf5ca1b6d515c09dfe0490ec37d4bf972fc53
Anibal Copitan <anibal.copitan@altimea.com>
2017-11-28 23:14:22 -0500

    [add]: home responsive xs and sm

--- app/wp-content/themes-developing/classicwhite/src/home.php
+++ app/wp-content/themes-developing/classicwhite/src/home.php
@@ -76,12 +76,12 @@
 			</div>
 
 			<div class="row justify-content-center mt-4">
-				<div class="col-9 col-sm-12 col-md-10">
+				<div class="col-12 col-sm-12 col-md-10">
 					<section class="regular slider-product">
 						<?php for ( $i = 0; $i < 6; $i++ ) : ?>
 							<div>
 								<a class="card-product" href="#">
-									<img src="http://placehold.it/350x300?text=1" class="img-fluid" />
+									<img src="http://placehold.it/240x240?text=1" class="_img-fluid" />
 									<h3 class="my-2">Product Name</h3>
 									<p>Es un hecho establecido hace demasiado tiempo que un lector se distraerá</p>
 								</a>
@@ -100,8 +100,17 @@
 					<h2>CONOCE MÁS SOBRE TORRINO</h2>
 				</div>
 			</div>
-			<div class="row">
-				pagess pages
+			<div class="row justify-content-center mt-4">
+				<div class="col-12 col-sm-12 col-md-10 d-flex justify-content-left flex-wrap">
+					<?php for ( $i = 0; $i < 3; $i++ ) : ?>
+						<div class="">
+							<a href="#" class="card-page text-center">
+								<img src="http://placehold.it/240x240?text=2" alt="" class="img-fluid_" />
+								<h3 class="mt-3">CONOCE LA HISTORIA DE TORINO EL PAYASO PANADERO</h3>
+							</a>
+						</div>
+					<?php endfor;?>
+				</div>
 			</div>
 		</div>
 	</section>
